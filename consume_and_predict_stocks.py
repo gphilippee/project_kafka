07d15ff0302f700efc3a5606505f51843002ab56
Vincent-Tch <vincent.tchoumba@telecom-sudparis.eu>
2023-01-12 23:06:58 +0100

    Update online Learning

--- consume_and_predict_stocks.py
+++ consume_and_predict_stocks.py
@@ -70,7 +70,47 @@ def calculate_scores(y_true, y_holt, y_snarimax):
         rmse_holt.update(yt, y_holt)
         rmse_snarimax.update(yt, y_snarimax)
 
-    return rmse_holt, rmse_snarimax
+    return rmse_holt.get(), rmse_snarimax.get()
+
+
+def consumer_model(models, metrics, forcasts, value, forcast_dates, y_true, idx=0):
+    """
+
+    :param models (list): list of the models to train
+    :param metrics (list): Metrics to use to evaluate each model, len(metrics) == len(models)
+    :param forcasts (list):
+    :param idx (int): 
+    :param value (float64):
+    :return:
+    """
+    models[0] = models[0].learn_one(value)
+    models[1] = models[1].learn_one(value)
+
+    # we need retain in memory the true values in order to calculate metrics
+    if y_true is not None:
+        if len(y_true) < horizon:
+            y_true.append(value)
+        else:
+            score_holt, score_snarimax = calculate_scores(
+                y_true, forcasts[0], forcasts[1]
+            )
+            metrics[0].append(score_holt)
+            metrics[1].append(score_snarimax)
+            plot_scores(forcast_dates, metrics[0], metrics[1])
+            plot_results(
+                forcast_dates[-1], y_true, forcasts[0], forcasts[1]
+            )
+            y_true = None
+
+    # no predict before 365
+    # we will predict each 120 days
+    if idx >= 365 and idx % forecast_delta == 0:
+        forcast_dates.append(date)
+        forcasts[0], forcasts[1] = predict(models[0], models[1])
+        y_true = []
+
+    idx += 1  # update index
+
 
 
 def make_predictions():
@@ -83,19 +123,21 @@ def make_predictions():
     )
 
     # Define 1st model
-    holt_model = time_series.HoltWinters(alpha=0.3, beta=0.1, gamma=0.6, seasonality=24)
+    holt_model_sp500 = time_series.HoltWinters(alpha=0.3, beta=0.1, gamma=0.6, seasonality=24)
+    holt_model_cac40 = time_series.HoltWinters(alpha=0.3, beta=0.1, gamma=0.6, seasonality=24)
+    holt_model_nikkei225 = time_series.HoltWinters(alpha=0.3, beta=0.1, gamma=0.6, seasonality=24)
+
 
     # Define 2nd model
     # If d=0, ARMA, else ARIMA
-    snarimax_model = time_series.SNARIMAX(p=2, d=1, q=1)
-
-    # to plot evolution of MSE through time
-    forecast_dates = []  # array for dates of forecast
-    rmse_holt_list = []
-    rmse_snarimax_list = []
+    snarimax_model_sp500 = time_series.SNARIMAX(p=2, d=1, q=1)
+    snarimax_model_cac40 = time_series.SNARIMAX(p=2, d=1, q=1)
+    snarimax_model_nikkei225 = time_series.SNARIMAX(p=2, d=1, q=1)
 
     idx_sp500 = 0
-    y_true = None
+    idx_cac40 = 0
+    idx_nikkei225 = 0
+
     for message in consumer_indexes:
         date = pd.to_datetime(message.timestamp, unit="s").date()  # get date
         close = message.value["Close"]
@@ -104,35 +146,55 @@ def make_predictions():
             continue
 
         if message.topic == sp500_topic:  # If message comes from S&P500 topic
-            holt_model = holt_model.learn_one(close)
-            snarimax_model = snarimax_model.learn_one(close)
 
-            # we need retain in memory the true values in order to calculate metrics
-            if y_true is not None:
-                if len(y_true) < horizon:
-                    y_true.append(close)
-                else:
-                    mse_holt, mse_snarimax = calculate_scores(
-                        y_true, forecast_holt, forecast_snarimax
-                    )
-                    rmse_holt_list.append(mse_holt.get())
-                    rmse_snarimax_list.append(mse_snarimax.get())
-                    plot_scores(forecast_dates, rmse_holt_list, rmse_snarimax_list)
-                    plot_results(
-                        forecast_dates[-1], y_true, forecast_holt, forecast_snarimax
-                    )
-                    y_true = None
+            y_true_sp500 = None
+            metrics_sp500 = [[],[]]
+            forecasts_sp500 = [np.array([]), np.array([])]
+            forecast_dates_sp500 = list()
 
-            # no predict before 365
-            # we will predict each 120 days
-            if idx_sp500 >= 365 and idx_sp500 % forecast_delta == 0:
-                forecast_dates.append(date)
-                forecast_holt, forecast_snarimax = predict(holt_model, snarimax_model)
-                y_true = []
+            consumer_model(
+                models = [holt_model_sp500, snarimax_model_sp500],
+                metrics = metrics_sp500,
+                forcasts = forecasts_sp500,
+                value = close,
+                forcast_dates = forecast_dates_sp500,
+                y_true = y_true_sp500,
+                idx = idx_sp500
+            )
 
-            idx_sp500 += 1  # update index
-        else:  # TODO: implement models for other topics
-            pass
+        elif message.topic == cac40_topic:  # TODO: implement models for other topics
+            
+            y_true_cac40  = None
+            metrics_cac40 = [[],[]]
+            forecasts_cac40 = [np.array([]), np.array([])]
+            forecast_dates_cac40 = list()
+
+            consumer_model(
+                models = [holt_model_cac40, snarimax_model_cac40],
+                metrics = metrics_cac40,
+                forcasts = forecasts_cac40,
+                value = close,
+                forcast_dates = forecast_dates_cac40,
+                y_true = y_true_cac40,
+                idx = idx_cac40
+            )
+
+        else:
+
+            y_true_nikkei225 = None
+            metrics_nikkei225 = [[],[]]
+            forecasts_nikkei225 = [np.array([]), np.array([])]
+            forecast_dates_nikkei225 = list()
+
+            consumer_model(
+                models = [holt_model_nikkei225, snarimax_model_nikkei225],
+                metrics = metrics_nikkei225,
+                forcasts = forecasts_nikkei225,
+                value = close,
+                forcast_dates = forecast_dates_nikkei225,
+                y_true = y_true_nikkei225,
+                idx = idx_nikkei225
+            )
 
 
 if __name__ == "__main__":
@@ -142,7 +204,7 @@ if __name__ == "__main__":
     nikkei225_topic = "nikkei225"
 
     horizon = 30  # forecast of horizon days
-    forecast_delta = 365  # days between 2 forecast
+    forecast_delta = 120  # days between 2 forecast
     assert forecast_delta > horizon, "forecast_delta should be superior to horizon"
 
     if not os.path.exists("stocks_forecast"):
