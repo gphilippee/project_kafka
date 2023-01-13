ea40508a1a01a2c5ef6c27c6524bb8023e551a73
Guillaume Philippe <guillaume.philippe2000@gmail.com>
2023-01-13 10:16:37 +0100

    to object-oriented

--- consume_and_predict_stocks.py
+++ consume_and_predict_stocks.py
@@ -9,108 +9,98 @@ import json
 from kafka import KafkaConsumer
 from river import time_series, metrics
 import os
+from typing import List, Union
 
 
-def plot_results(date, y_true, y_pred_holt, y_pred_snarimax):
-    plt.figure(figsize=(15, 8))
-    plt.plot(y_pred_holt, "r-", label="Predictions HoltWinters")
-    plt.plot(y_pred_snarimax, "y-", label="Predictions SNARIMAX")
-    plt.plot(y_true, "g-", label="True Values")
-    # to avoid flat plot if forecast goes to far
-    # plt.ylim(bottom=0, top=min(10_000, np.max([y_pred_holt, y_pred_snarimax]) + 500))
-    plt.legend()
-    plt.grid()
-    plt.savefig(base_plot_file + f"_{date:%Y-%m-%d}.png")
-    plt.close()
-
-
-def plot_scores(forecast_dates, rmse_holt_list, rmse_snarimax_list):
-    # Plot to see the impact of training the model on MSE
-    plt.figure()
-    plt.title(f"MSE through time")
-    plt.yscale("log")
-    plt.plot(forecast_dates, rmse_holt_list, label="HoltWinters")
-    plt.plot(forecast_dates, rmse_snarimax_list, label="SNARIMAX")
-    plt.xlabel("Date")
-    plt.ylabel("MSE")
-    plt.legend()
+def plot_metrics_models(models):
+    nb_models = len(models)
+    fig, ax = plt.subplots(nb_models, 1, figsize=(15, 10))
+    for i in range(nb_models):
+        ax[i].set_title(f"MSE through time")
+        ax[i].plot.plot(
+            models[i].forecast_dates, models[i].rmse_holt_list, label="HoltWinters"
+        )
+        ax[i].plot.plot(
+            models[i].forecast_dates, models[i].rmse_snarimax_list, label="SNARIMAX"
+        )
+        ax[i].set_title(models[i].name)
+        ax[i].legend()
+        ax[i].set_xlabel("Date")
     plt.savefig(mse_plot_file)
     plt.close()
 
 
-def predict(holt_model, snarimax_model):
-    """
+class StreamModels:
+    def __init__(self, topic, horizon, forecast_delta):
+        self.holt_model = time_series.HoltWinters(
+            alpha=0.3, beta=0.1, gamma=0.6, seasonality=24
+        )
+        self.snarimax_model = time_series.SNARIMAX(p=2, d=1, q=1)
+        self.horizon: int = horizon
+        self.forecast_delta: int = forecast_delta
+        self.topic: str = topic
+        self.rmse_holt_list: List[float] = []
+        self.rmse_snarimax_list: List[float] = []
+        self.y_true_memory: Union[List, None] = None
+        self.idx: int = 0
+        self.forecast_dates: List = []
+        self.forecast_holt = []
+        self.forecast_snarimax = []
 
-    :param holt_model:
-    :param snarimax_model:
-    :return:
-    """
-    forecast_holt = holt_model.forecast(horizon=horizon)
-    forecast_snarimax = snarimax_model.forecast(horizon=horizon)
+    def _forecast(self):
+        forecast_holt = self.holt_model.forecast(horizon=horizon)
+        forecast_snarimax = self.snarimax_model.forecast(horizon=horizon)
 
-    # A stock can't go below 0
-    forecast_snarimax = np.maximum(forecast_snarimax, 0)
-    forecast_holt = np.maximum(forecast_holt, 0)
+        # A stock can't go below 0
+        self.forecast_holt = np.maximum(forecast_holt, 0)
+        self.forecast_snarimax = np.maximum(forecast_snarimax, 0)
 
-    return forecast_holt, forecast_snarimax
+    def _scores(self):
+        rmse_holt = metrics.RMSE()
+        rmse_snarimax = metrics.RMSE()
 
+        for yt, yh, ys in zip(
+            self.y_true_memory, self.forecast_holt, self.forecast_snarimax
+        ):
+            rmse_holt.update(yt, yh)
+            rmse_snarimax.update(yt, ys)
 
-def calculate_scores(y_true, y_holt, y_snarimax):
-    """
+        self.rmse_holt_list.append(rmse_holt.get())
+        self.rmse_snarimax_list.append(rmse_snarimax.get())
 
-    :param y_true:
-    :param y_holt:
-    :param y_snarimax:
-    :return:
-    """
-    rmse_holt = metrics.RMSE()
-    rmse_snarimax = metrics.RMSE()
+    def train(self, x, date):
+        # learn_one on models
+        self.holt_model = self.holt_model.learn_one(x)
+        self.snarimax_model = self.snarimax_model.learn_one(x)
 
-    for yt, y_holt, y_snarimax in zip(y_true, y_holt, y_snarimax):
-        rmse_holt.update(yt, y_holt)
-        rmse_snarimax.update(yt, y_snarimax)
+        # add to memory OR calculate scores and reset memory
+        if self.y_true_memory is not None and len(self.y_true_memory) < horizon:
+            self.y_true_memory.append(x)
+        elif self.y_true_memory is not None:
+            self._scores()
+            self._plot_forecasts(date)
+            self.y_true_memory = None
 
-    return rmse_holt.get(), rmse_snarimax.get()
+        # predict and set memory
+        if self.idx >= 365 and self.idx % self.forecast_delta == 0:
+            self._forecast()
+            self.forecast_dates.append(date)
+            self.y_true_memory = []
+        self.idx += 1
 
-
-def consumer_model(models, metrics, forcasts, value, forcast_dates, y_true, idx=0):
-    """
-
-    :param models (list): list of the models to train
-    :param metrics (list): Metrics to use to evaluate each model, len(metrics) == len(models)
-    :param forcasts (list):
-    :param idx (int): 
-    :param value (float64):
-    :return:
-    """
-    models[0] = models[0].learn_one(value)
-    models[1] = models[1].learn_one(value)
-
-    # we need retain in memory the true values in order to calculate metrics
-    if y_true is not None:
-        if len(y_true) < horizon:
-            y_true.append(value)
-        else:
-            score_holt, score_snarimax = calculate_scores(
-                y_true, forcasts[0], forcasts[1]
-            )
-            metrics[0].append(score_holt)
-            metrics[1].append(score_snarimax)
-            plot_scores(forcast_dates, metrics[0], metrics[1])
-            plot_results(
-                forcast_dates[-1], y_true, forcasts[0], forcasts[1]
-            )
-            y_true = None
-
-    # no predict before 365
-    # we will predict each 120 days
-    if idx >= 365 and idx % forecast_delta == 0:
-        forcast_dates.append(date)
-        forcasts[0], forcasts[1] = predict(models[0], models[1])
-        y_true = []
-
-    idx += 1  # update index
-
+    def _plot_forecasts(self, date):
+        plt.figure(figsize=(15, 8))
+        plt.plot(self.forecast_holt, "r-", label="Predictions HoltWinters")
+        plt.plot(self.forecast_snarimax, "y-", label="Predictions SNARIMAX")
+        plt.plot(self.y_true_memory, "g-", label="True Values")
+        # to avoid flat plot if forecast goes to far
+        # plt.ylim(bottom=0, top=min(10_000, np.max([y_pred_holt, y_pred_snarimax]) + 500))
+        plt.legend()
+        plt.grid()
+        plt.savefig(
+            plot_folder + f"{self.topic}_forecast_{horizon}_on_{date:%Y-%m-%d}.png"
+        )
+        plt.close()
 
 
 def make_predictions():
@@ -122,21 +112,15 @@ def make_predictions():
         value_deserializer=lambda m: json.loads(m.decode()),
     )
 
-    # Define 1st model
-    holt_model_sp500 = time_series.HoltWinters(alpha=0.3, beta=0.1, gamma=0.6, seasonality=24)
-    holt_model_cac40 = time_series.HoltWinters(alpha=0.3, beta=0.1, gamma=0.6, seasonality=24)
-    holt_model_nikkei225 = time_series.HoltWinters(alpha=0.3, beta=0.1, gamma=0.6, seasonality=24)
+    sp500_models = StreamModels(sp500_topic, horizon, forecast_delta)
+    cac40_models = StreamModels(cac40_topic, horizon, forecast_delta)
+    nikkei225_models = StreamModels(nikkei225_topic, horizon, forecast_delta)
 
-
-    # Define 2nd model
-    # If d=0, ARMA, else ARIMA
-    snarimax_model_sp500 = time_series.SNARIMAX(p=2, d=1, q=1)
-    snarimax_model_cac40 = time_series.SNARIMAX(p=2, d=1, q=1)
-    snarimax_model_nikkei225 = time_series.SNARIMAX(p=2, d=1, q=1)
-
-    idx_sp500 = 0
-    idx_cac40 = 0
-    idx_nikkei225 = 0
+    topic2model = {
+        sp500_topic: sp500_models,
+        cac40_topic: cac40_models,
+        nikkei225_topic: nikkei225_models,
+    }
 
     for message in consumer_indexes:
         date = pd.to_datetime(message.timestamp, unit="s").date()  # get date
@@ -145,56 +129,9 @@ def make_predictions():
         if not close:
             continue
 
-        if message.topic == sp500_topic:  # If message comes from S&P500 topic
-
-            y_true_sp500 = None
-            metrics_sp500 = [[],[]]
-            forecasts_sp500 = [np.array([]), np.array([])]
-            forecast_dates_sp500 = list()
-
-            consumer_model(
-                models = [holt_model_sp500, snarimax_model_sp500],
-                metrics = metrics_sp500,
-                forcasts = forecasts_sp500,
-                value = close,
-                forcast_dates = forecast_dates_sp500,
-                y_true = y_true_sp500,
-                idx = idx_sp500
-            )
-
-        elif message.topic == cac40_topic:  # TODO: implement models for other topics
-            
-            y_true_cac40  = None
-            metrics_cac40 = [[],[]]
-            forecasts_cac40 = [np.array([]), np.array([])]
-            forecast_dates_cac40 = list()
-
-            consumer_model(
-                models = [holt_model_cac40, snarimax_model_cac40],
-                metrics = metrics_cac40,
-                forcasts = forecasts_cac40,
-                value = close,
-                forcast_dates = forecast_dates_cac40,
-                y_true = y_true_cac40,
-                idx = idx_cac40
-            )
-
-        else:
-
-            y_true_nikkei225 = None
-            metrics_nikkei225 = [[],[]]
-            forecasts_nikkei225 = [np.array([]), np.array([])]
-            forecast_dates_nikkei225 = list()
-
-            consumer_model(
-                models = [holt_model_nikkei225, snarimax_model_nikkei225],
-                metrics = metrics_nikkei225,
-                forcasts = forecasts_nikkei225,
-                value = close,
-                forcast_dates = forecast_dates_nikkei225,
-                y_true = y_true_nikkei225,
-                idx = idx_nikkei225
-            )
+        if message.topic in topic2model.keys():
+            topic2model[message.topic].train(close, date)
+            # plot_metrics_models(topic2model.values())
 
 
 if __name__ == "__main__":
@@ -210,7 +147,7 @@ if __name__ == "__main__":
     if not os.path.exists("stocks_forecast"):
         os.mkdir("stocks_forecast")
 
-    base_plot_file = f"stocks_forecast/{horizon}_days_forecast"
+    plot_folder = f"stocks_forecast/"
     mse_plot_file = "mse_through_time.png"
 
     make_predictions()
