3d6960ecef5d32b782bbee71bdd675794786207b
Guillaume Philippe <guillaume.philippe2000@gmail.com>
2023-01-13 11:20:54 +0100

    new run + improve script

--- consume_and_predict_stocks.py
+++ consume_and_predict_stocks.py
@@ -10,23 +10,27 @@ from kafka import KafkaConsumer
 from river import time_series, metrics
 import os
 from typing import List, Union
+import datetime
 
 
 def plot_metrics_models(models):
     nb_models = len(models)
     fig, ax = plt.subplots(nb_models, 1, figsize=(15, 10))
     for i in range(nb_models):
-        ax[i].set_title(f"MSE through time")
-        ax[i].plot.plot(
+        ax[i].set_title(f"Metrics through time")
+        ax[i].plot(
             models[i].forecast_dates, models[i].rmse_holt_list, label="HoltWinters"
         )
-        ax[i].plot.plot(
+        ax[i].plot(
             models[i].forecast_dates, models[i].rmse_snarimax_list, label="SNARIMAX"
         )
-        ax[i].set_title(models[i].name)
+        ax[i].plot(models[i].forecast_dates, models[i].rmse_cst_list, label="Constant")
+        ax[i].set_title(models[i].topic)
         ax[i].legend()
         ax[i].set_xlabel("Date")
-    plt.savefig(mse_plot_file)
+        ax[i].set_ylim(0, models[i].max_value * 1.1)
+    plt.tight_layout()
+    plt.savefig(metrics_plot_file)
     plt.close()
 
 
@@ -44,6 +48,7 @@ class StreamModels:
         self.idx: int = 0
         self.horizon: int = horizon
         self.forecast_delta: int = forecast_delta
+        self.max_value: int = 0
         # define models
         self.cst_model = ConstantModel()
         self.snarimax_model = time_series.SNARIMAX(p=2, d=1, q=1)
@@ -71,7 +76,7 @@ class StreamModels:
         self.forecast_holt = np.maximum(forecast_holt, 0)
         self.forecast_snarimax = np.maximum(forecast_snarimax, 0)
 
-    def _scores(self):
+    def _scores(self, date):
         rmse_holt = metrics.RMSE()
         rmse_snarimax = metrics.RMSE()
         rmse_cst = metrics.RMSE()
@@ -86,11 +91,15 @@ class StreamModels:
             rmse_snarimax.update(yt, ys)
             rmse_cst.update(yt, yc)
 
+        self.forecast_dates.append(date - datetime.timedelta(days=30))
         self.rmse_holt_list.append(rmse_holt.get())
         self.rmse_snarimax_list.append(rmse_snarimax.get())
         self.rmse_cst_list.append(rmse_cst.get())
 
     def train(self, x, date):
+        if x > self.max_value:
+            self.max_value = x
+
         # learn_one on models
         self.holt_model = self.holt_model.learn_one(x)
         self.snarimax_model = self.snarimax_model.learn_one(x)
@@ -99,14 +108,13 @@ class StreamModels:
         if self.y_true_memory is not None and len(self.y_true_memory) < horizon:
             self.y_true_memory.append(x)
         elif self.y_true_memory is not None:
-            self._scores()
+            self._scores(date)
             self._plot_forecasts(date)
             self.y_true_memory = None
 
         # predict and set memory
         if self.idx >= 365 and self.idx % self.forecast_delta == 0:
             self._forecast(x)
-            self.forecast_dates.append(date)
             self.y_true_memory = []
         self.idx += 1
 
@@ -116,8 +124,6 @@ class StreamModels:
         plt.plot(self.forecast_snarimax, "y-", label="Predictions SNARIMAX")
         plt.plot(self.forecast_cst, "b-", label="Predictions Constant")
         plt.plot(self.y_true_memory, "g-", label="True Values")
-        # to avoid flat plot if forecast goes to far
-        # plt.ylim(bottom=0, top=min(10_000, np.max([y_pred_holt, y_pred_snarimax]) + 500))
         plt.legend()
         plt.grid()
         plt.savefig(
@@ -145,16 +151,22 @@ def make_predictions():
         nikkei225_topic: nikkei225_models,
     }
 
-    for message in consumer_indexes:
-        date = pd.to_datetime(message.timestamp, unit="s").date()  # get date
-        close = message.value["Close"]
+    launch_date = datetime.datetime.now().date()
+
+    for msg in consumer_indexes:
+        date = pd.to_datetime(msg.timestamp, unit="s").date()  # get date
+        close = msg.value["Close"]
 
         if not close:
             continue
 
-        if message.topic in topic2model.keys():
-            topic2model[message.topic].train(close, date)
-            # plot_metrics_models(topic2model.values())
+        if msg.topic in topic2model.keys():
+            topic2model[msg.topic].train(close, date)
+
+        # plot at the end
+        if date >= launch_date:
+            plot_metrics_models(list(topic2model.values()))
+            break
 
 
 if __name__ == "__main__":
@@ -171,6 +183,6 @@ if __name__ == "__main__":
         os.mkdir("stocks_forecast")
 
     plot_folder = f"stocks_forecast/"
-    mse_plot_file = "mse_through_time.png"
+    metrics_plot_file = "metrics_through_time.png"
 
     make_predictions()
